1996fc5a415b2897f3774a1a6975c82db8edf636
Eve Cho <e.young.echo@gmail.com>
2022-08-12 02:58:06 -0400

    making routes, finished error handling in index.js although i will probably backtrack and clean it up

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ const app = express();
 module.exports = app; // this line is only used to make testing easier.
 
 // remember to plug in your router and any other middleware you may need here (i.e. body parser, mounting any router-level middleware, etc.)
+app.use('/users', require('./routes/index'));
+app.use(express.urlencoded({extended: false}));
 
 app.use((err, req, res, next) => {
+  console.error(err.stack);
   res.sendStatus(err.status);
 });
 
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,47 @@
 const express = require('express');
 const router = express.Router();
+const app = require('../app');
 const todos = require('../models/express-models/todos');
 module.exports = router;
 
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 // write your routes here. Feel free to split into multiple files if you like.
+
+router.get('/', (req, res, next) => {
+    try {
+        res.send(todos.listPeople());
+    } catch(err) {
+        console.error(err);
+        next();
+    }
+})
+
+router.get('/:name/tasks', (req, res, next) => {
+    try {
+        if (!todos.listPeople().includes(req.params.name)) {
+            res.status(404);
+            throw new Error('page not found 404');
+        }
+        res.send(todos.list(req.params.name));
+    } catch (err) {
+        console.error(err);
+        next();
+    }
+})
+
+
+router.post('/:name/tasks', (req, res, next) => {
+    try {
+        if (req.body.content === '') {
+            res.sendStatus(400);
+        }
+        todos.add(req.params.name, req.body);
+        res.type('application/json')
+        res.status(201).json(todos.list(req.params.name)[0]);
+    } catch(err) {
+        console.log('\n\n\nwe got an error!')
+        console.error(err);
+        next();
+    }
+})
