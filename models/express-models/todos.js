547da2ed3b57a8fee834d7aee87be8f293a57ece
Eve Cho <e.young.echo@gmail.com>
2022-08-12 01:17:13 -0400

    started checkpoint: finished todos.js, but may return to it later to refactor add

--- models/express-models/todos.js
+++ models/express-models/todos.js
@@ -18,21 +18,45 @@ module.exports = {
   // ==== COMPLETE THE FOLLOWING (SEE `model.js` TEST SPEC) =====
   listPeople: function () {
     // returns an array of all people for whom tasks exist
+    return Object.keys(tasks);
+
   },
 
   add: function (name, task) {
     // saves a task for a given person
+    if (tasks[name] !== undefined) {
+      // if name is already an existing key, pushes the task to their value
+      // (their value is the tasks array)
+      if (task.complete !== undefined) {
+        // if the given task does have a completion status
+        tasks[name].push(task);
+      } else {
+        tasks[name].push({content: task.content, complete: false});
+      }
+    } else {
+      // if the name is not an existing key, initialize a new key-value
+      // set it equal to an array, and then push the task to the new property
+      tasks[name] = [];
+      if (task.complete !== undefined) {
+        tasks[name].push(task);
+      } else {
+        tasks[name].push({content: task.content, complete: false});
+      }
+    }
   },
 
   list: function (name) {
     // returns tasks for specified person
+    return tasks[name];
   },
 
   complete: function (name, idx) {
     // marks a task complete
+    tasks[name][idx].complete = true;
   },
 
   remove: function (name, idx) {
     // removes a tasks
+    tasks[name].splice(idx, 1);
   },
 };
