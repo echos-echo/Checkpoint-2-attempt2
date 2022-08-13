201320365148d8fba4e5421495f397f7ad3d26c5
Eve Cho <e.young.echo@gmail.com>
2022-08-12 20:53:21 -0400

    submission for deadline:/ sequelize-models/index.js refactored and commented. finally complete...

--- models/sequelize-models/index.js
+++ models/sequelize-models/index.js
@@ -5,6 +5,7 @@ const Sequelize = require('sequelize');
 
 //---------VVVV---------  your code below  ---------VVV----------
 
+// instantiating the model Task which connects to table, Task
 const Task = db.define('Task', {
   name: {
     type: Sequelize.STRING,
@@ -20,6 +21,7 @@ const Task = db.define('Task', {
   due: Sequelize.DATE,
 });
 
+// Class method: will delete all rows where completion status (complete) is true
 Task.clearCompleted = async () => {
   await Task.destroy({
     where: {
@@ -28,6 +30,7 @@ Task.clearCompleted = async () => {
   })
 }
 
+// Class method: changes complete status for all rows to true
 Task.completeAll = async () => {
   await Task.update(
     { complete: true },
@@ -35,25 +38,25 @@ Task.completeAll = async () => {
     )
 }
 
+// Instance method: returns time remainind until the task is due
 Task.prototype.getTimeRemaining = function() {
-  if (this.due === undefined) {
-    return Infinity;
-  } else {
-    return this.due - Date.now();
-  }
+  // if there is no due time on the task, return Infinity; otherwise returns the time remaining
+  return this.due === undefined ? Infinity : this.due - Date.now();
 }
 
+// Instance method: returns true if the task is overdue, and false if there is time left/complete on time
 Task.prototype.isOverdue = function() {
+  // difference between the (hopefully) future due date and current time
+  // if dueStatus is negative, that means the due date already passed...
   const dueStatus = this.due - Date.now();
-  if (dueStatus > 0) {
-    return false;
-  } else if (dueStatus < 0 && this.complete === true) {
+  if (dueStatus > 0 || (dueStatus < 0 && this.complete)) {
     return false;
   } else if (dueStatus < 0) {
     return true;
   }
 }
 
+// Instance method: assigns an owner (id) to a task (foreignKey: OwnerId)
 Task.prototype.assignOwner =  async function(owner) {
   const task = this;
   task.OwnerId = owner.id;
@@ -62,6 +65,7 @@ Task.prototype.assignOwner =  async function(owner) {
   });
 }
 
+// instantiating the model Owner which connects to table, Owner
 const Owner = db.define('Owner', {
   name: {
     type: Sequelize.STRING,
@@ -72,12 +76,14 @@ const Owner = db.define('Owner', {
   },
 });
 
+// Class method: returns all owners and their corresponding tasks
 Owner.getOwnersAndTasks = () => {
   return Owner.findAll({
     include: Task
   })
 }
 
+// Instance method: returns all of the owner's (this) tasks which are incomplete
 Owner.prototype.getIncompleteTasks = async function() {
   const incomplete = await Owner.findOne(
     { include:
@@ -90,6 +96,7 @@ Owner.prototype.getIncompleteTasks = async function() {
   return incomplete.Tasks;
 }
 
+// Hook on Owner: before trying to destroy any owner, checks through this first...
 Owner.beforeDestroy(owner => {
   if (owner.name === 'Grace Hopper') {
     throw new Error('You cannot destroy Grace Hopper!')
@@ -99,6 +106,7 @@ Owner.beforeDestroy(owner => {
 Task.belongsTo(Owner);
 Owner.hasMany(Task, {
   foreignKey: 'OwnerId'
+  // associates Task and Owner such that Task creates the foreignKey 'OwnerId' to refer to an owner
 });
 
 
